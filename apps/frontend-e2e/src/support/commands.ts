0c0dcbb4669a17989f6af65851c651a8e976c2d5
Timothy Pierce <disdainful777@gmail.com>
2024-06-10 06:40:19 -0600

    Update commands.ts

--- apps/frontend-e2e/src/support/commands.ts
+++ apps/frontend-e2e/src/support/commands.ts
@@ -14,12 +14,12 @@
 declare namespace Cypress {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   interface Chainable<Subject> {
-    login(email: string, password: string): void
+    login(email: string, password_data: string): void
   }
 }
 
 // -- This is a parent command --
-Cypress.Commands.add('login', (email, password) => {
+Cypress.Commands.add('login', (email, password_data) => {
   console.log('Custom command example: Login', email, password)
 })
 //
